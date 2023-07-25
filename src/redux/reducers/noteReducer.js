7cc1acbf455f446dcdbaa028bb08474f84653b19
Reetik Basniwal <reetikbasniwal@gmail.com>
2023-07-25 12:29:02 +0530

    reducer using createSlice from redux-toolkit

--- src/redux/reducers/noteReducer.js
+++ src/redux/reducers/noteReducer.js
@@ -1,32 +1,59 @@
-import { ADD_NOTE, DELETE_NOTE } from "../actions/noteAction";
+import { createSlice } from "@reduxjs/toolkit";
+// import { ADD_NOTE, DELETE_NOTE } from "../actions/noteAction";
 
 const initialState = {
   notes: [
-    { text: "Coding in morning is very important, You complete every task that you decided to complete to get the Job not a Job, you have to get a good JOB", createdOn: new Date() },
-    { text: "Go to gym at evening or morning but exercise is very important, this body is given to us by lord and our duty to keep this working", createdOn: new Date() },
+    {
+      text: "Coding in morning is very important, You complete every task that you decided to complete to get the Job not a Job, you have to get a good JOB",
+      createdOn: new Date(),
+    },
+    {
+      text: "Go to gym at evening or morning but exercise is very important, this body is given to us by lord and our duty to keep this working",
+      createdOn: new Date(),
+    },
   ],
 };
 
-export function noteReducer(state = initialState, action) {
-  switch (action.type) {
-    case ADD_NOTE:
-      return {
-        ...state,
-        notes: [
-          ...state.notes,
-          {
-            text: action.text,
-            createdOn: new Date(),
-          },
-        ],
-      };
-    case DELETE_NOTE:
-      state.notes.splice(action.index, 1);
-      return {
-        ...state,
-        notes: [...state.notes]
-      };
-    default:
-      return state;
-  }
-}
+const noteSlice = createSlice({
+  name: "note",
+  initialState: initialState,
+  reducers: {
+    // this is ADD NOTE ACTION
+    addNote: (state, action) => {
+      state.notes.push({
+        text: action.payload,
+        createdOn: new Date(),
+      });
+    },
+
+    // this is DELETE NOTE ACTION
+    deleteNote: (state, action) => {
+      state.notes.splice(action.payload, 1);
+    },
+  },
+});
+
+export const noteReducer = noteSlice.reducer;
+// export function noteReducer(state = initialState, action) {
+//   switch (action.type) {
+//     case ADD_NOTE:
+//       return {
+//         ...state,
+//         notes: [
+//           ...state.notes,
+//           {
+//             text: action.text,
+//             createdOn: new Date(),
+//           },
+//         ],
+//       };
+//     case DELETE_NOTE:
+//       state.notes.splice(action.index, 1);
+//       return {
+//         ...state,
+//         notes: [...state.notes]
+//       };
+//     default:
+//       return state;
+//   }
+// }
--- src/redux/reducers/todoReducer.js
+++ src/redux/reducers/todoReducer.js
@@ -1,37 +1,67 @@
-import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
+import { createSlice } from "@reduxjs/toolkit";
+// import { ADD_TODO, TOGGLE_TODO } from "../actions/todoActions";
 
 const initialState = {
-    todos:[
-        {"text": "Coding in morning", completed: false},
-        {"text": "Go to gym at evening", completed: true}
-    ]
+  todos: [
+    { text: "Coding in morning", completed: false },
+    { text: "Go to gym at evening", completed: true },
+  ],
 };
 
-export function todoReducer(state=initialState, action){
-    switch(action.type){
-        case ADD_TODO:
-            return {
-                ...state,
-                todos: [
-                    ...state.todos,
-                    {
-                        text: action.text,
-                        completed: false
-                    }
-                ]
-                
-            }
-        case TOGGLE_TODO:
-            return {
-                ...state,
-                todos:  state.todos.map((todo, i) => {
-                    if(i === action.index){
-                        todo.completed = !todo.completed
-                    }
-                    return todo;
-                })
-            }
-        default:
-            return state;
-    }
-}
+// REDUCER  USING REDUX TOOLKIT
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: initialState,
+  reducers: {
+    // this is ADD ACTION
+    add: (state, action) => {
+      state.todos.push({
+        text: action.payload,
+        completed: false,
+      });
+    },
+    // this is TOGGLE ACTION
+    toggle: (state, action) => {
+      state.todos.map((todo, i) => {
+        if (i === action.payload) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+    },
+  },
+});
+
+export const todoReducer = todoSlice.reducer;
+
+// REDUCER USING REDUX
+
+// export function todoReducer(state=initialState, action){
+//     switch(action.type){
+//         case ADD_TODO:
+//             return {
+//                 ...state,
+//                 todos: [
+//                     ...state.todos,
+//                     {
+//                         text: action.text,
+//                         completed: false
+//                     }
+//                 ]
+
+//             }
+//         case TOGGLE_TODO:
+//             return {
+//                 ...state,
+//                 todos:  state.todos.map((todo, i) => {
+//                     if(i === action.index){
+//                         todo.completed = !todo.completed
+//                     }
+//                     return todo;
+//                 })
+//             }
+//         default:
+//             return state;
+//     }
+// }
