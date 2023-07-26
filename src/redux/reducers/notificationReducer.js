57df74c88b22f999f73a442e4b9759265de2fd1d
Reetik Basniwal <reetikbasniwal@gmail.com>
2023-07-26 12:16:57 +0530

    notfication for Notes

--- src/redux/reducers/notificationReducer.js
+++ src/redux/reducers/notificationReducer.js
@@ -1,42 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 // import { actions } from "./todoReducer"
-import { todoActions } from "./todoReducer"
-import { noteActions } from "./noteReducer"
-
+import { todoActions } from "./todoReducer";
+import { noteActions } from "./noteReducer";
 
 const initialState = {
-    message: "" 
-}
-
+  message: "",
+};
 
 const notificationSlice = createSlice({
-    name:'notification',
-    initialState,
-    reducers: {
-        reset: (state, action) => {
-            state.message = ""
-        }
+  name: "notification",
+  initialState,
+  reducers: {
+    reset: (state, action) => {
+      state.message = "";
     },
-    // extraReducers:{
-    //     "todo/add":(state, action) => {
-    //         state.message = "Todo is added"
-    //     },
-    //     "note/addNote":(state, action) => {
-    //         state.message = "Note is added"
-    //     }
-    // }
-    extraReducers: (builder) => {
-        builder.addCase(todoActions.add, (state, action) => {
-            state.message = "Todo is created";
-        });
-        builder.addCase(noteActions.addNote, (state, action) => {
-            state.message = "Note is created";
-        })
-    }
-})
+  },
+  // NOT A CORRECT WAY
+  // extraReducers:{
+  //     "todo/add":(state, action) => {
+  //         state.message = "Todo is added"
+  //     },
+  //     "note/addNote":(state, action) => {
+  //         state.message = "Note is added"
+  //     }
+  // }
+  // -------------------------
+  // CORRECT WAY
+  // extraReducers: (builder) => {
+  //     builder.addCase(todoActions.add, (state, action) => {
+  //         state.message = "Todo is created";
+  //     });
+  //     builder.addCase(noteActions.addNote, (state, action) => {
+  //         state.message = "Note is created";
+  //     })
+  // }
+  extraReducers: {
+    [todoActions.add]: (state, action) => {
+      state.message = "Todo is created";
+    },
+    [noteActions.addNote]: (state, action) => {
+      state.message = "Note is created";
+    },
+  },
+});
 
 export const notificationReducer = notificationSlice.reducer;
 
-export const notificationSelector = (state) => state.notificationReducer.message;
+export const notificationSelector = (state) =>
+  state.notificationReducer.message;
 
 export const resetNotification = notificationSlice.actions.reset;
